0e297d32a47b2dea2c34440249ac7a902ee35090
Richard Mahn <richard_mahn@wycliffeassociates.org>
2016-09-26 09:49:46 -0600

    Updates to all handlers

--- aws_tools/s3_handler.py
+++ aws_tools/s3_handler.py
@@ -8,18 +8,23 @@
 #  Richard Mahn <richard_mahn@wycliffeassociates.org>
 
 import os
+import boto3
 
 from boto3.session import Session
 
 
 class S3Handler(object):
     
-    def __init__(self, aws_access_key_id, aws_secret_access_key, aws_region_name='us-west-2'):
-        self.session = Session(aws_access_key_id=aws_access_key_id,
-                               aws_secret_access_key=aws_secret_access_key,
-                               region_name=aws_region_name)
-        self.client = self.session.client('s3')
-        self.resource = self.session.resource('s3')
+    def __init__(self, aws_access_key_id=None, aws_secret_access_key=None, aws_region_name='us-west-2'):
+        if aws_access_key_id and aws_secret_access_key:
+            session = Session(aws_access_key_id=aws_access_key_id,
+                                   aws_secret_access_key=aws_secret_access_key,
+                                   region_name=aws_region_name)
+            self.resource = session.resource('s3')
+            self.resource = session.resource('s3')
+        else:
+            self.resource = boto3.resource('s3')
+            self.client = boto3.client('s3')
 
     # Downloads all the files in S3 that have a prefix of `dist` from `bucket` to the `local` directory
     def download_dir(self, bucket, dist, local):
@@ -32,5 +37,5 @@ class S3Handler(object):
                 for f in result.get('Contents'):
                     if not os.path.exists(os.path.dirname(local + os.sep + f.get('Key'))):
                         os.makedirs(os.path.dirname(local + os.sep + f.get('Key')))
-                    self.resource.meta.self.client.download_file(bucket, f.get('Key'), local + os.sep + f.get('Key'))
+                    self.resource.meta.client.download_file(bucket, f.get('Key'), local + os.sep + f.get('Key'))
 
